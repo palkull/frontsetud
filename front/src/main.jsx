6c88a679c1d1d0693fb854560104cc05be1fd307
palkull <palkulx2000@gmail.com>
2025-07-03 10:29:34 -0600

    Fix import paths for DeleteUser and Addusers components

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.jsx";
 import "./index.css";
 import Usuarios from "./pages/inicio/usuarios.jsx";
-import DeleteUser from "./components/deleteusers.jsx";
-import Addusers from "./components/addusers.jsx";
+import DeleteUser from "./components/users/deleteusers.jsx";
+import Addusers from "./components/users/addusers.jsx";
 
 const root = document.getElementById("root");
 
@@ -16,7 +16,6 @@ ReactDOM.createRoot(root).render(
 
       <Route path="/usuarios" element={<Usuarios />}>
         <Route index element={<DeleteUser />} />
-
         <Route path="add" element={<Addusers />} />
       </Route>
     </Routes>
