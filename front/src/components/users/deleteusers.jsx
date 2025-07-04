450b79bc1d711ec4c04f255e870719a41215fdaf
palkull <palkulx2000@gmail.com>
2025-07-04 11:44:48 -0600

    Add Nav component with responsive design; include CSS for styling

--- front/src/components/users/deleteusers.jsx
+++ front/src/components/users/deleteusers.jsx
@@ -1,6 +1,10 @@
+import Nav from "../nav/nav";
+
 function DeleteUser() {
     return (
         <>
+              <Nav />
+
             <h1>Delete User</h1>
             <p>Are you sure you want to delete this user?</p>
         </>
