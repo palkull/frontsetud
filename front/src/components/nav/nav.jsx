82db235cc360e4e6d81349c13970e0134e99dced
palkull <palkulx2000@gmail.com>
2025-07-16 10:24:11 -0600

    Add logo to navigation and update menu links for better clarity and styling
    - Replaced the text "Navbar" with a logo image.
    - Updated navigation links to "Mi cuenta" and "Documentos" for improved relevance.
    - Enhanced link styles with hover effects for better user experience.
    - Adjusted mobile menu layout to use a grid for better organization.

--- front/src/components/nav/nav.jsx
+++ front/src/components/nav/nav.jsx
@@ -1,5 +1,6 @@
 // import './nav.css';
 import { Link } from "react-router-dom"; // Asegúrate de usar 'react-router-dom' en lugar de 'react-router'
+import Logo from "../../assets/SETUED.svg"; // Asegúrate de que la ruta al logo sea correcta
 
 function Nav() {
   return (
@@ -8,14 +9,14 @@ function Nav() {
         <div className="flex justify-between h-16 items-center">
           {/* Logo o marca */}
           <div className="flex-shrink-0 text-xl font-bold text-gray-800 dark:text-white">
-            Navbar
+            <img className="w-50" src={Logo} alt="Logo" />
           </div>
 
           {/* Menú de navegación */}
           <div className="hidden md:flex space-x-4">
-            <Link to="/inicio">Home</Link>
-            <Link to="/features">Features</Link>
-            <Link to="/pricing">Pricing</Link>
+            <Link className="font-bold text-blue-500 hover:scale-110 transition-transform duration-200 hover:text-blue-700" to="/inicio">Home</Link>
+            <Link className="font-bold text-blue-500 hover:scale-110 transition-transform duration-200 hover:text-blue-700" to="/features">Mi cuenta</Link>
+            <Link className="font-bold text-blue-500 hover:scale-110 transition-transform duration-200 hover:text-blue-700" to="/pricing">Documentos</Link>
           </div>
 
           {/* Botón de menú hamburguesa para móviles */}
@@ -42,10 +43,10 @@ function Nav() {
       </div>
 
       {/* Menú desplegable en dispositivos móviles */}
-      <div className="md:hidden hidden px-4 pt-2 pb-4 space-y-1" id="mobile-menu">
-        <Link to="/">Home</Link>
-        <Link to="/features">Features</Link>
-        <Link to="/pricing">Pricing</Link>
+      <div className="md:hidden hidden px-4 pt-2 pb-4 space-y-1 grid grid-rows-3 " id="mobile-menu">
+        <Link className=" p-1 font-bold text-blue-500 hover:scale-110 transition-transform duration-200 hover:text-blue-700 text-center" to="/">Home</Link>
+        <Link className=" p-1 font-bold text-blue-500 hover:scale-110 transition-transform duration-200 hover:text-blue-700 text-center" to="/features">Mi cuenta</Link>
+        <Link className=" p-1 font-bold text-blue-500 hover:scale-110 transition-transform duration-200 hover:text-blue-700 text-center" to="/pricing">Documentos</Link>
       </div>
     </nav>
   );
