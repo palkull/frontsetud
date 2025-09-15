81e829af934260da8662bab261969e03d262402f
palkull <palkulx2000@gmail.com>
2025-09-15 14:42:56 -0600

    refactor: simplify axios instance configuration and add request interceptor

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "https://back-setued.onrender.com/",
-  withCredentials: true,
+axios.defaults.baseURL = "https://back-setued.onrender.com";
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-})
-
-export default instance;
+export default axios;
